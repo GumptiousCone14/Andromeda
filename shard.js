b6edb5c86280af356ae5a517c6ae9c57d91eb5c5
GumptiousCone14 <meanderingcanary8365@gmail.com>
2020-12-29 15:48:45 +0000

    main

--- shard.js
+++ shard.js
@@ -1,9 +1,8 @@
-// index.js
-const Discord = require('discord.js');
-const moment = require('moment');
 // sharding.js
+const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./app.js');
+const moment = require('moment');
 
-Manager.spawn(3);
-Manager.on('launch', shard => console.log(`- Spawned shard ${shard.id} --- ${moment().utc().format('MMMM Do')}, ${moment().utc().format('hh:mm a')} --`));
-
+Manager.spawn(1);
+Manager.on('launch', shard => console.log(`- Spawned shard ${shard.id} -`));
+console.log(`-- ${moment().utc().format('MMMM Do')}, ${moment().utc().format('hh:mm a')} --`);
